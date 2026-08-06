2043b2721066c8a354eadd7691be4e7fd7f1c1d1
massatrio16 <massatrioo09@gmail.com>
2026-08-06 09:54:12 +0700

    initial

---
+++ twrp_X668C.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+DEVICE_PATH := device/infinix/X668C
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from itel-S665L device
+$(call inherit-product, device/infinix/X668C/device.mk)
+
+
+
+
+PRODUCT_DEVICE := X668C
+PRODUCT_NAME := twrp_X668C
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X668C
+PRODUCT_MANUFACTURER := infinix
+PRODUCT_RELEASE_NAME := Infinix X668C
+
+
