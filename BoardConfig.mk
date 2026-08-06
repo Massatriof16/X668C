e8eaa9f25f6cd0cd861f1bd90efad542b10b3757
massatrio16 <massatrioo09@gmail.com>
2026-08-06 12:25:36 +0700

    test

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,20 +12,20 @@ ALLOW_MISSING_DEPENDENCIES := true
 
 #TW_LOAD_VENDOR_MODULES := "adaptive-ts.ko focaltech_ft8756_spi_ts.ko"
 
+
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor \
+    vendor \
     dtbo \
+    system_ext \
+    vbmeta_vendor \
+    vbmeta \
     boot \
     system \
-    system_ext \
-    vendor \
-    product \
-    vendor_dlkm
-
+    vbmeta_system \
+    vendor_dlkm \
+    product
 
 
 #vendor_boot configuration
@@ -42,34 +42,33 @@ BOARD_RAMDISK_USE_LZ4 := true
 # Assert
 TARGET_OTA_ASSERT_DEVICE := Infinix X668C
 
-# Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+TARGET_CPU_VARIANT_RUNTIME := generic
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+TARGET_2ND_CPU_VARIANT_RUNTIME := generic
 
-TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules)\")
+#TW_LOAD_VENDOR_MODULES := $(shell echo \"$(shell ls $(DEVICE_PATH)/recovery/root/lib/modules)\")
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := Infinix-X668C
 TARGET_NO_BOOTLOADER := true
 
 # Display
-TARGET_SCREEN_DENSITY := 400
+TARGET_SCREEN_DENSITY := 320
 
 # MKBOOTIMG from original vendor_boot 
 BOARD_VENDOR_BASE := 0x00000000
 # TODO: remove "androidboot.." from kernel cmdline after verification
-BOARD_VENDOR_CMDLINE :=  console=ttyS1,115200n8 buildvariant=user
+BOARD_VENDOR_CMDLINE :=  'console=ttyS1,115200n8 buildvariant=user'
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x05400000
 BOARD_KERNEL_OFFSET := 0x00008000
@@ -116,19 +115,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 # System as root
 BOARD_SUPPRESS_SECURE_ERASE := true
 
-# Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_VBMETA_SYSTEM := system system_ext product vendor
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 4
-BOARD_AVB_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_BOOT_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_BOOT_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 4
-BOARD_AVB_VENDOR_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_VENDOR_BOOT_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_VENDOR_BOOT_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
-BOARD_AVB_VENDOR_BOOT_ROLLBACK_INDEX_LOCATION := 1
 
 # Platform
 BOARD_USES_UNISOC_HARDWARE := true
@@ -152,11 +138,17 @@ TW_USE_FSCRYPT_POLICY := 2
 
 # Hack: prevent anti rollback
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2021-08-01
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
+# Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 99.87.36
-PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
-
+PLATFORM_VERSION := 16.1.0
 # TWRP Configuration
 
 TW_HAS_DOWNLOAD_MODE := true
