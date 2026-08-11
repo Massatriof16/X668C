6c9d45629d93b279f16865491b161d3ba6b362a7
massatrio16 <massatrioo09@gmail.com>
2026-08-12 05:12:23 +0700

    try

--- twrp_X668C.mk
+++ twrp_X668C.mk
@@ -9,7 +9,8 @@ DEVICE_PATH := device/infinix/X668C
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
